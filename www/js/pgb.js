983a8919cf50a3bd4b6e05af0bb1101cc4f297a7
Natalia Lewczuk <Natalia Lewczuk>
2017-03-27 23:39:04 +0200

    sprawdzenie połacznena

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,23 +6,18 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function deviceInfo() {
+function checkConnection() {
+    var networkState = navigator.connection.type;
 
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Nieznane połączenie';
+    states[Connection.ETHERNET] = 'Ethernet';
+    states[Connection.WIFI]     = 'WiFi';
+    states[Connection.CELL_2G]  = 'Siec 2G';
+    states[Connection.CELL_3G]  = 'Siec 3G';
+    states[Connection.CELL_4G]  = 'Siec 4G';
+    states[Connection.CELL]     = 'Siec';
+    states[Connection.NONE]     = 'Brak polaczenia';
 
-	navigator.notification.alert(info);
-	
-}
-
-function personalInfo(){
-	
-	navigator.notification.alert("Natalia Lewczuk");
-	
+    navigator.notification.alert('Rodzaj polaczenia: ' + states[networkState]);
 }
